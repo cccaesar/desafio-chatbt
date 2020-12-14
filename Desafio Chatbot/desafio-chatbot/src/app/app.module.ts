605347383d711cab7efc83a442ab2611e17f4d01
unknown <ccsrrdrgscosta@gmail.com>
2020-12-14 13:05:02 -0300

    Adicionado um modulo para o login e os modulos ReactiveFormsModule e FormsModule estão sendo importados pelo app.module

--- Desafio Chatbot/desafio-chatbot/src/app/app.module.ts
+++ Desafio Chatbot/desafio-chatbot/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './pages/login/login.component';
+import { LoginModule } from './pages/login/login.module'
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
